5795d2010422ac7ba272cb65cb33adc5980989ff
SUNNY BHAVEEN CHANDRA <34603575+c17hawke@users.noreply.github.com>
2023-06-07 10:01:50 +0530

    update as per fastAPI

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
-from flask import Flask, render_template, request, jsonify
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 import openai
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-app = Flask(__name__)
+app = FastAPI()
+static_path = "static"
+app.mount("/static", StaticFiles(directory=static_path), name="static")
+templates = Jinja2Templates(directory="templates")
 
-@app.route("/")
-def index():
-    return render_template('chat.html')
-
+@app.get("/")
+def index(request: Request):
+    return templates.TemplateResponse("chat.html", {"request": request})
 
 def get_completion(prompt, model="gpt-3.5-turbo"):
     messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=0, 
+        model = model,
+        messages = messages
     )
-    return response.choices[0].message["content"]
+    return response.choices[0].message.content
 
-
-@app.route("/get", methods=["GET", "POST"])
-def chat():
-    msg = request.form["msg"]
-    input = msg
+@app.post("/get")
+async def chat(request: Request):
+    msg = await request.form()
+    input = msg["msg"]
     return get_completion(input)
 
 if __name__ == '__main__':
-    app.run()
+    uvicorn.run(app, host="0.0.0.0", port=8000)
